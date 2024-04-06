06040c3558bf4a046072ea32dfc1cf76821b0ace
Dacounter95 <146789960+germanhytsystem@users.noreply.github.com>
2024-04-06 02:55:09 -0500

    feat: New config for project

--- Config/Config.php
+++ Config/Config.php
@@ -1,18 +1,18 @@
 <?php
      //OJO con el base_url
-    const BASE_URL = "http://localhost/Proyectos/7_2_pos_venta_prod/";
-    const HOST = "localhost";
-    const BD = "pos_mvc";
-    const DB_USER = "root";
-    const PASS = "";
-    const CHARSET = "charset=utf8";
-
-    // const BASE_URL = "http://shermandeploy.byethost7.com/7_2_pos_venta_prod/";
-    // const HOST = "sql108.byethost7.com";
-    // const BD = "b7_34312610_pos_mvc";
-    // const DB_USER = "b7_34312610";
-    // const PASS = "9R0PD4S9";
+    // const BASE_URL = "http://localhost/Proyectos/7_2_pos_venta_prod/";
+    // const HOST = "localhost";
+    // const BD = "pos_mvc";
+    // const DB_USER = "root";
+    // const PASS = "";
     // const CHARSET = "charset=utf8";
+
+    const BASE_URL = "http://shermansystems.byethost8.com/7_2_pos_venta_prod/";
+    const HOST = "sql106.byethost8.com";
+    const BD = "b8_36299315_pos_mvc";
+    const DB_USER = "b8_36299315";
+    const PASS = "9R0PD4S9";
+    const CHARSET = "charset=utf8";
 ?>
 
 
